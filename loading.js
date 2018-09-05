f7c4ae2f687934316e57808abf7676870be7e8b9
Shivam Thapliyal <thapliyalshivam@gmail.com>
2018-09-05 23:16:41 +0530

    first loop

--- loading.js
+++ loading.js
@@ -1,3 +1,6 @@
+
+window.addEventListener('load', loadModule);
+
 async function loadModule() {
   const num = window.location.hash.substr(1) || 1;
   const module = await
@@ -15,45 +18,3 @@ async function init() {
     }
     module = await loadModule();
   }
-
-  const capturer = new CCapture({
-    verbose: false,
-    display: true,
-    framerate: 60,
-    motionBlurFrames: 0 * (960 / 60),
-    quality: 99,
-    format: 'gif',
-    timeLimit: module.loopDuration,
-    frameLimit: 0,
-    autoSaveTime: 0,
-    workersPath: 'js/'
-  });
-
-  let startTime = 0;
-
-  function capture() {
-    capturer.start();
-    startTime = performance.now();
-  }
-
-  document.getElementById('start').addEventListener('click', e => {
-    capture();
-    e.preventDefault();
-  });
-
-  function update() {
-    requestAnimationFrame(update);
-    if (!skip) module.draw(startTime);
-    capturer.capture(module.canvas);
-  }
-
-  update();
-
-  window.addEventListener('hashchange', async e => {
-    reload();
-  })
-}
-
-window.skip = false;
-
-window.addEventListener('load', loadModule);
