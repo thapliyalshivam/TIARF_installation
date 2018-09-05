de9369ff4eaed24e6bf4db5078811c3cf92ed7c0
Shivam Thapliyal <thapliyalshivam@gmail.com>
2018-09-06 00:20:38 +0530

    first loop

--- loading.js
+++ loading.js
@@ -2,13 +2,13 @@ async function loadModule() {
   const num = window.location.hash.substr(1) || 1;
   const module = await
   import (`./js/main_${num}.js`);
+   document.body.appendChild(module.domElement);
 return module
 
 }
 
 function initalize(){
   var mod = loadModule();
-    document.body.appendChild(mod.domElement);
 }
 
 window.addEventListener('load', initalize);
